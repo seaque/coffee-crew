9311b43947c8ba29f785fb86907de011f71dca19
seaque <45668726+seaque@users.noreply.github.com>
2022-04-19 15:05:27 +0300

    Initial commit

--- lib/models/coffee.dart
+++
@@ -1,8 +0,0 @@
-class Coffee {
-  late String name;
-  late String choice;
-  late int sugars;
-  late int strength;
-
-  Coffee({required this.name, required this.choice, required this.sugars, required this.strength});
-}
